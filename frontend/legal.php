1f71dd7be789dd7f09fb62e944f8a6b629e7f273
fernk2938 <sleachrdlel@gmail.com>
2025-07-02 15:39:32 +0200

    Create legal.php

---
+++ frontend/legal.php
@@ -0,0 +1,5 @@
+<h2>Mentions légales</h2>
+<p>
+  En utilisant ce site, vous acceptez que les réponses générées par l'IA puissent contenir du contenu non filtré.<br>
+  Vous êtes seul responsable de leur usage. Aucune donnée personnelle n’est conservée par le système.
+</p>
